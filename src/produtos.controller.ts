07c91577975d06be1337c9ce219ec02d055e695e
Bruna Lopes <bruna-chan@live.com>
2022-01-17 21:51:27 -0300

    feat: using product entity

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -3,35 +3,45 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
+import { Produto } from './produto.model';
 
 @Controller('produtos')
 export class ProdutosController {
+  produtos: Produto[] = [
+    new Produto('LV0001', 'Livro show de bola', 45.99),
+    new Produto('LV0002', 'Outro livro massa', 78.56),
+    new Produto('LV0003', 'Livro top demais', 32.12),
+  ];
+
   @Get()
-  index(): string {
-    return 'Obter lista de produtos';
+  index(): Produto[] {
+    return this.produtos;
   }
 
   @Get(':id')
-  show(@Param() params): string {
-    return `Devolve os dados do produto ${params.id}`;
+  show(@Param() params): Produto {
+    return Produto[0];
   }
 
   @Post()
-  store(@Body() produto): string {
-    return 'Produto criado';
+  store(@Body() produto: Produto): void {
+    produto.id = 4;
+    this.produtos.push(produto);
   }
 
   @Put(':id')
-  update(@Param() params, @Body() produto): string {
-    return 'Produto atualizado';
+  update(@Param() params, @Body() produto: Produto): Produto {
+    return produto;
   }
 
   @Delete(':id')
-  destroy(@Param() params): string {
-    return 'Produto removido';
+  @HttpCode(204)
+  destroy(@Param() params): void {
+    this.produtos.pop();
   }
 }
