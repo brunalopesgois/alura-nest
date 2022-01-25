6fbb1908058d1b07d9d01282c0ac7d1bdfef8b05
Bruna L Gois <bruna-chan@live.com>
2022-01-25 13:26:05 -0300

    feat: using dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'nest_livraria',
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
       autoLoadModels: true,
       synchronize: true,
     }),
